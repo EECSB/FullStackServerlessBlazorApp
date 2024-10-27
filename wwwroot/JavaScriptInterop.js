387b7988d7008c9893c2f0eee8c90be10ce09800
EECSB <50377992+EECSB@users.noreply.github.com>
2024-10-27 20:24:30 +0100

    Add project files.

---
+++ wwwroot/JavaScriptInterop.js
@@ -0,0 +1,22 @@
+﻿function DownloadFile(filename, contentType, content) {
+    //Create the URL.
+    const file = new File([content], filename, { type: contentType });
+    const exportUrl = URL.createObjectURL(file);
+    //Creates <a> element and clicks on it programmatically.
+    const a = document.createElement("a");
+    document.body.appendChild(a);
+    a.href = exportUrl;
+    a.download = filename;
+    a.target = "_self";
+    a.click();
+    //Remove URL object after clicking on it.
+    URL.revokeObjectURL(exportUrl);
+}
+
+function CopyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function () {
+        console.log('Text copied to clipboard');
+    }, function (err) {
+        console.error('Could not copy text: ', err);
+    });
+};
